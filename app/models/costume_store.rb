aafabd759d7e2102a15317027952d0f7cf1a7fe9
github username <github email address>
2019-05-23 23:40:12 +0100

    Done.

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,1 +1,6 @@
 # Create your CostumeStore class here
+class CostumeStore < ActiveRecord::Base
+
+   # attr_accessor :name, :location, :costume_inventory, :number_of_employees, :in_business, :opening, :closing
+
+end
